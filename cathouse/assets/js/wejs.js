007e585c02b937bba09fee34e0add49b47c35d26
Voltokk <vladlexxx@yahoo.com>
2017-11-08 00:45:50 +0200

    3 pages added

---
+++ cathouse/assets/js/wejs.js
@@ -0,0 +1,1 @@
+document.write( '<script src="https://www.tripadvisor.co.uk/WidgetEmbed-cdsratingsonlywide?border=false&backgroundColor=white&shadow=false&locationId=1160010&display_version=2&uniq=637&lang=en_UK"></script>' );
